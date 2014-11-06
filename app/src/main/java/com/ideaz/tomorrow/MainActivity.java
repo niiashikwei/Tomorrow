164fdd495ae7eee0a55ec8d0606461b7049d7ade
Nii <niiashikwei@hotmail.com>
2014-11-06 07:49:03 -0800

    Nii - added splash screen with animation

--- app/src/main/java/com/ideaz/tomorrow/MainActivity.java
+++ app/src/main/java/com/ideaz/tomorrow/MainActivity.java
@@ -4,14 +4,32 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.RotateAnimation;
 
 
 public class MainActivity extends Activity {
+    protected AlphaAnimation fadeIn = new AlphaAnimation(0.0f , 1.0f);
+    protected AlphaAnimation fadeOut = new AlphaAnimation( 1.0f , 0.0f);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        kickOffAnimation();
+
+    }
+
+    private void kickOffAnimation() {
+        View splashTextView = findViewById(R.id.splashText);
+        splashTextView.startAnimation(fadeIn);
+        splashTextView.startAnimation(fadeOut);
+        fadeIn.setDuration(2000);
+        fadeIn.setFillAfter(true);
+        fadeOut.setDuration(4000);
+        fadeOut.setFillAfter(true);
+        fadeOut.setStartOffset(3000+fadeIn.getStartOffset());
     }
 
 
